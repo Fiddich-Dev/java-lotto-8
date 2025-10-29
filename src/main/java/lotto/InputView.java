a493c2480cb7a2a67b9a5644ebf40af2e1a52dd6
Fiddich <hiws99@naver.com>
2025-10-29 21:32:58 +0900

    feat(input): 입력값을 받는 view 구현
    뒤에 구현할 parser의 단위테스트를 위해 입력화면에서는 데이터 가공을 하지 않음

---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public String requestPurchaseAmount() {
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+        String input = Console.readLine();
+        return input;
+    }
+
+    public String requestWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_MESSAGE);
+        String input = Console.readLine();
+        return input;
+    }
+
+    public String requestBonusNumber() {
+        System.out.println(BONUS_NUMBER_MESSAGE);
+        String input = Console.readLine();
+        return input;
+    }
+}
